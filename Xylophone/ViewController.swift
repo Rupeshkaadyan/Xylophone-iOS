f50078885b6da6efdedbb842032df3dc843741d7
Rupeshkaadyan <134736518+Rupeshkaadyan@users.noreply.github.com>
2025-12-28 13:12:30 +0530

    Enhance button interaction with animation and haptic feedback

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,20 +1,84 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: Any) {
+        animateButton(sender as! UIButton)
+        playSound("C")
+        hapticFeedback()
+    }
+    
+    @IBAction func keyPressed2(_ sender: Any) {
+        animateButton(sender as! UIButton)
+        playSound("D")
+        hapticFeedback()
+    }
+    
+    @IBAction func keyPressedE(_ sender: Any) {
+        animateButton(sender as! UIButton)
+        playSound("E")
+        hapticFeedback()
+    }
+    
+    @IBAction func keyPressedF(_ sender: Any) {
+        animateButton(sender as! UIButton)
+        playSound("F")
+        hapticFeedback()
+    }
+    
+    @IBAction func keyPressedG(_ sender: Any) {
+        animateButton(sender as! UIButton)
+        playSound("G")
+        hapticFeedback()
+    }
+    
+    @IBAction func keyPressedA(_ sender: Any) {
+        animateButton(sender as! UIButton)
+        hapticFeedback()
+        playSound("A")
+    }
+    
+    @IBAction func keyPressedB(_ sender: Any) {
+        animateButton(sender as! UIButton)
+        playSound("B")
+        hapticFeedback()
+    }
+    
+    
+    func playSound(_ soundName: String) {
+        if let url = Bundle.main.url(forResource: soundName, withExtension: "wav") {
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                player?.play()
+            } catch {
+                print("Error playing sound")
+            }
+        }
+    } // so this is my music one fun
+    
+    func animateButton(_ button: UIButton) {
+        UIView.animate(withDuration: 0.1,
+                       animations: {
+            button.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        }) { _ in
+            UIView.animate(withDuration: 0.1) {
+                button.transform = .identity
+            }
+        }
+    }
+    func hapticFeedback() {
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.impactOccurred()
+    }
+    
     
 
 }
