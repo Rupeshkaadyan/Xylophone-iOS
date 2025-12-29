73acc3d92788beb8abd80ddfc0491f4781659851
Rupeshkaadyan <134736518+Rupeshkaadyan@users.noreply.github.com>
2025-12-29 07:33:44 +0530

    Refactor xylophone buttons to use single IBAction with dynamic sound playback

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,85 +1,35 @@
-
-
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+
     var player: AVAudioPlayer?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func keyPressed(_ sender: UIButton) {
+        animateButton(sender)
+        hapticFeedback()
+
+        if let note = sender.currentTitle {
+            playSound(note)
+        }
     }
 
-    @IBAction func keyPressed(_ sender: Any) {
-        animateButton(sender as! UIButton)
-        playSound("C")
-        hapticFeedback()
-    }
-    
-    @IBAction func keyPressed2(_ sender: Any) {
-        animateButton(sender as! UIButton)
-        playSound("D")
-        hapticFeedback()
-    }
-    
-    @IBAction func keyPressedE(_ sender: Any) {
-        animateButton(sender as! UIButton)
-        playSound("E")
-        hapticFeedback()
-    }
-    
-    @IBAction func keyPressedF(_ sender: Any) {
-        animateButton(sender as! UIButton)
-        playSound("F")
-        hapticFeedback()
-    }
-    
-    @IBAction func keyPressedG(_ sender: Any) {
-        animateButton(sender as! UIButton)
-        playSound("G")
-        hapticFeedback()
-    }
-    
-    @IBAction func keyPressedA(_ sender: Any) {
-        animateButton(sender as! UIButton)
-        hapticFeedback()
-        playSound("A")
-    }
-    
-    @IBAction func keyPressedB(_ sender: Any) {
-        animateButton(sender as! UIButton)
-        playSound("B")
-        hapticFeedback()
-    }
-    
-    
     func playSound(_ soundName: String) {
         if let url = Bundle.main.url(forResource: soundName, withExtension: "wav") {
-            do {
-                player = try AVAudioPlayer(contentsOf: url)
-                player?.play()
-            } catch {
-                print("Error playing sound")
-            }
+            player = try? AVAudioPlayer(contentsOf: url)
+            player?.play()
         }
-    } // so this is my music one fun
-    
+    }
+
     func animateButton(_ button: UIButton) {
-        UIView.animate(withDuration: 0.1,
-                       animations: {
+        UIView.animate(withDuration: 0.1, animations: {
             button.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
         }) { _ in
-            UIView.animate(withDuration: 0.1) {
-                button.transform = .identity
-            }
+            button.transform = .identity
         }
     }
+
     func hapticFeedback() {
-        let generator = UIImpactFeedbackGenerator(style: .light)
-        generator.impactOccurred()
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
     }
-    
-    
-
 }
-
